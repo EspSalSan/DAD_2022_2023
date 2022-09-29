e166a048220c9143bc70634a4c041e31d022cc20
Diogo Santos <diogosantoss360@gmail.com>
2022-09-30 00:22:18 +0100

    now with functions ;-)

--- BoneyBank/PuppetMaster/Program.cs
+++ BoneyBank/PuppetMaster/Program.cs
@@ -8,7 +8,7 @@ namespace PuppetMaster
     {
         static string GetSolutionDir()
         {
-            // ...\BoneyBank
+            // Leads to /BoneyBank
             return Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent?.Parent?.FullName;
         }
 
@@ -23,16 +23,50 @@ namespace PuppetMaster
             Process.Start(p_info);
         }
 
+        static void CreateProcess(string[] configArgs)
+        {
+            // Not MacOS friendly ;-)
+            string baseDirectory = GetSolutionDir();
+            string clientPath = Path.Combine(baseDirectory, "BankClient", "bin", "Debug", "netcoreapp3.1", "BankClient.exe");
+            string serverPath = Path.Combine(baseDirectory, "BankServer", "bin", "Debug", "netcoreapp3.1", "BankServer.exe");
+            string boneyPath = Path.Combine(baseDirectory, "Boney", "bin", "Debug", "netcoreapp3.1", "Boney.exe");
+
+            string id = configArgs[1];
+            string name = configArgs[2];
+
+            if (name.Equals("client"))
+            {
+                StartProcess(clientPath, id);
+            }
+            else if (name.Equals("boney") || name.Equals("bank"))
+            {
+                string url = configArgs[3].Remove(0, 7);
+                string host = url.Split(':')[0];
+                string port = url.Split(':')[1];
+
+                Console.WriteLine(id + " " + host + " " + port);
+
+                if (name.Equals("bank"))
+                {
+                    StartProcess(serverPath, id + " " + host + " " + port);
+                }
+                else
+                {
+                    StartProcess(boneyPath, id + " " + host + " " + port);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect config file");
+                return;
+            }
+        }
+
         static void Main(string[] args)
         {
 
             string baseDirectory = GetSolutionDir();
             string configFilePath = Path.Join(baseDirectory, "PuppetMaster", "config.txt");
-
-            string clientPath = Path.Combine(baseDirectory, "BankClient", "bin", "Debug", "netcoreapp3.1", "BankClient.exe");
-            string serverPath = Path.Combine(baseDirectory, "BankServer", "bin", "Debug", "netcoreapp3.1", "BankServer.exe");
-            string boneyPath = Path.Combine(baseDirectory, "Boney", "bin", "Debug", "netcoreapp3.1", "Boney.exe");
-
 
             if (!File.Exists(configFilePath))
             {
@@ -43,41 +77,30 @@ namespace PuppetMaster
             string[] lines = File.ReadAllLines(configFilePath);
             foreach(string line in lines)
             {
-
                 string[] configArgs = line.Split(" ");
 
-                if (configArgs[0].Equals("P"))
+                switch (configArgs[0])
                 {
-                    string id = configArgs[1];
-                    string name = configArgs[2];
+                    case "P":
+                        CreateProcess(configArgs);
+                        break;
 
-                    if (name.Equals("client"))
-                    {
-                        StartProcess(clientPath, id);
-                    } 
-                    else if (name.Equals("boney") || name.Equals("bank"))
-                    {
-                        string url = configArgs[3].Remove(0,7);
-                        string host = url.Split(':')[0];
-                        string port = url.Split(':')[1];
+                    case "S":
+                        break;
 
-                        Console.WriteLine(id + " " + host + " " + port);
+                    case "T":
+                        break;
 
-                        if (name.Equals("bank"))
-                        {
-                            StartProcess(serverPath, id + " " + host + " " + port);
-                        } 
-                        else
-                        {
-                            StartProcess(boneyPath, id + " " + host + " " + port);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Incorrect config file");
-                        return;
-                    }
-                } // s t d f
+                    case "D":
+                        break;
+
+                    case "F":
+                        break;
+
+                    default:
+                        break;
+
+                }
             }    
         }
     }

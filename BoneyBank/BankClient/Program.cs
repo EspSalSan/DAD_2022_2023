433a7e2f8e24b26401c5c2ccad3f8cb2f7db1ed2
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-10-05 16:45:30 +0100

    Using ReadConfig on BankClients

--- BoneyBank/BankClient/Program.cs
+++ BoneyBank/BankClient/Program.cs
@@ -3,94 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace BankClient
 {
     internal class Program
     {
-        static string GetSolutionDir()
-        {
-            // Leads to /BoneyBank
-            return Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent?.Parent?.FullName;
-        }
-        static Dictionary<string, Bank.BankClient> GetBankHost(string[] lines) 
-        {
-            // Search config for bank URLs
-            Dictionary<string, Bank.BankClient> bankHosts = new Dictionary <string,Bank.BankClient>();
-
-            foreach (string line in lines)
-            {
-                string[] configArgs = line.Split(" ");
-
-                if (configArgs[0].Equals("P") && configArgs[2].Equals("bank"))
-                {
-                    GrpcChannel channel = GrpcChannel.ForAddress(configArgs[3]);
-                    bankHosts.Add(configArgs[1], new Bank.BankClient(channel));
-                }
-            }
-            return bankHosts;
-        }
-
-        static List<Dictionary<int, string>> GetProcessesState(string[] lines)
-        {
-            // Search config for process state (suspected / not-suspected)
-            List<Dictionary<int, string>> processesStatePerSlot = new List<Dictionary<int, string>>();
-
-            // Regex to detect the triplet, e.g.: (1, N, NS)
-            string pattern = @"(\([^0-9]*\d+[^0-9]*\))";
-            Regex rg = new Regex(pattern);
-
-            foreach (string line in lines)
-            {
-                if (line[0].Equals('F'))
-                {
-                    var processesStateSlot = new Dictionary<int, string>(); 
-
-                    MatchCollection matched = rg.Matches(line);
-                    foreach (Match match in matched)
-                    {
-                        string[] values = match.Value.Split(", ");
-
-                        int processId = int.Parse(values[0].Remove(0,1));
-                        string state = values[2].Remove(values[2].Length-1);
-
-                        processesStateSlot.Add(processId, state);  
-                    }
-
-                    // FUTURE USE PRINT DICTIONARY VERY USEFULL
-                    //processesStateSlot.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
-                    processesStatePerSlot.Add(processesStateSlot);
-                }
-            }
-            return processesStatePerSlot;
-        }
-
-        static (int slotDuration, TimeSpan startTime) GetSlotsDetails(string[] lines)
-        {
-            int slotDuration = -1;
-            TimeSpan startTime = new TimeSpan();
-
-            foreach (string line in lines)
-            {
-                if (line[0].Equals('T'))
-                {
-                    string[] configArgs = line.Split(" ");
-                    string[] time = configArgs[1].Split(":");
-                    startTime = new TimeSpan(int.Parse(time[0]), int.Parse(time[1]), int.Parse(time[2]));
-                }
-                if (line[0].Equals('D'))
-                {
-                    string[] configArgs = line.Split(" ");
-                    slotDuration = int.Parse(configArgs[1]);
-                }
-            }
-            return (slotDuration, startTime);
-        }
-
-        static void SendDepositRequest(string[] commandArgs, Dictionary<string, Bank.BankClient> bankHosts)
+        static void SendDepositRequest(string[] commandArgs, Dictionary<int, Bank.BankClient> bankHosts)
         {
             // Verify arguments
             if (commandArgs.Length != 2)
@@ -115,7 +36,7 @@ namespace BankClient
             }
         }
 
-        static void SendWithdrawRequest(string[] commandArgs, Dictionary<string, Bank.BankClient> bankHosts)
+        static void SendWithdrawRequest(string[] commandArgs, Dictionary<int, Bank.BankClient> bankHosts)
         {
             // Verify arguments
             if (commandArgs.Length != 2)
@@ -140,7 +61,7 @@ namespace BankClient
             }
         }
 
-        static void SendReadBalanceRequest(string[] commandArgs, Dictionary<string, Bank.BankClient> bankHosts)
+        static void SendReadBalanceRequest(string[] commandArgs, Dictionary<int, Bank.BankClient> bankHosts)
         {
             // Verify arguments
             if (commandArgs.Length != 1)
@@ -170,16 +91,14 @@ namespace BankClient
         {
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            // Read config.txt
-            string baseDirectory = GetSolutionDir();
-            string configFilePath = Path.Join(baseDirectory, "PuppetMaster", "config.txt");
-            string[] lines = File.ReadAllLines(configFilePath);
-
             // Initial data
             int processId = int.Parse(args[0]);
-            Dictionary <string, Bank.BankClient> bankHosts = GetBankHost(lines);
-            List<Dictionary<int, string>> processesStatePerSlot = GetProcessesState(lines);
-            (int slotDuration, TimeSpan startTime) = GetSlotsDetails(lines);
+            BoneyBankConfig config = Common.ReadConfig();
+            Dictionary <int, Bank.BankClient> bankHosts = config.BankServers.ToDictionary(key => key.Id, value =>
+            {
+                GrpcChannel channel = GrpcChannel.ForAddress(value.Address);
+                return new Bank.BankClient(channel);
+            });
 
             Console.WriteLine($"Bank Client ({processId}) starting...");
 
--- BoneyBank/Utilities/Common.cs
+++ BoneyBank/Utilities/Common.cs
@@ -1,14 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Utilities
 {
+    // BankProcess struct with 3 fields (process id, type, and address)
+    public struct BankProcess
+    {
+        public int Id { get; }
+        public string Type { get; }
+        public string Address { get; }
+
+        // Constructor
+        public BankProcess(int id, string type, string address)
+        {
+            Id = id;
+            Type = type;
+            Address = address;
+        }
+    }
+
+    public struct BoneyBankConfig
+    {
+        public List<BankProcess> BankServers { get; }
+        public List<BankProcess> BoneyServers { get; }
+        public int NumberOfProcesses { get; }
+        
+        public BoneyBankConfig(List<BankProcess> bankServers, List<BankProcess> boneyServers, int numberOfProcesses)
+        {
+            BankServers = bankServers;
+            BoneyServers = boneyServers;
+            NumberOfProcesses = numberOfProcesses;
+        }
+    }
+    
     public static class Common
     {
         public static string GetSolutionDir()
         {
             // Leads to /BoneyBank
             return Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent?.Parent?.FullName;
+        }
+
+        public static BoneyBankConfig ReadConfig()
+        {
+            string configFilePath = Path.Join(GetSolutionDir(), "PuppetMaster", "config.txt");
+            string[] lines = File.ReadAllLines(configFilePath);
+            int numberOfProcesses = 0;
+            List<BankProcess> bankServers = new List<BankProcess>();
+            List<BankProcess> boneyServers = new List<BankProcess>();
+
+            foreach (string line in lines)
+            {
+                string[] args = line.Split(" ");
+
+                if (args[0].Equals("P") && !args[2].Equals("client"))
+                {
+                    numberOfProcesses++;
+                    int processId = int.Parse(args[1]);
+                    BankProcess bankProcess = new BankProcess(processId, args[2], args[3]);
+                    switch (args[2])
+                    {
+                        case "bank":
+                            bankServers.Add(bankProcess);
+                            break;
+                        case "boney":
+                            boneyServers.Add(bankProcess);
+                            break;
+                    }
+                }
+            }
+            
+            return new BoneyBankConfig(bankServers, boneyServers, numberOfProcesses);
         }
     }
 }
